cd93577b99d9aafb13c24d365d0221f3539d8c4c
ndamelio-sketch <ndamelio@udesa.edu.ar>
2025-11-17 17:04:59 -0300

    Inner HTML en Register y Log In

--- js/register.js
+++ js/register.js
@@ -3,18 +3,24 @@ let mail_reg = document.querySelector('#mail_reg')
 let pass_reg = document.querySelector('#pass_reg')
 let repeat_reg = document.querySelector('#repeat_reg')
 
+// Errores
+
+let mailError = document.querySelector('.invalid_mail')
+let passError = document.querySelector('.invalid_pass')
+let repeatError = document.querySelector('.invalid_repeat')
+
 formRegister.addEventListener('submit',function(event){
     event.preventDefault()
     if(mail_reg.value == ''){
-        alert('El mail no puede estar vacio.')
+        mailError.innerHTML = "<p>El mail no puede quedar vacío.</p>"
     }
     else if(pass_reg.value == '')
-        alert('La contraseña no puede estar vacia.')
+        passError.innerHTML = "<p>La contraseña no puede estar vacía.</p>"
     else if((pass_reg.value).length < 6){
-        alert('La contraseña debe tener al menos 6 caracteres.')
+        repeatError.innerHTML = "<p>La contraseña debe tener al menos 6 caracteres.</p>"
     }
     else if (pass_reg.value !== repeat_reg.value){
-        alert('Las contraseñas no coinciden.')
+        repeatError.innerHTML = "<p>Las contraseñas no coinciden.</p>"
     }
     else{
         this.submit()
