bb981d94c288856b755fe7da1f9c65fb33114c55
barberom <barberom@udesa.edu.ar>
2025-11-18 22:36:13 -0300

    index appi

--- js/index.js
+++ js/index.js
@@ -40,3 +40,25 @@ fetch('https://dummyjson.com/products/category/motorcycle')
 .catch(function(error){
     console.log(error)
 })
+
+fetch('https://dummyjson.com/products/category/vehicle')
+.then(function(response){
+    return response.json()
+})
+.then(function(data){
+    let otros_productos = document.querySelector('#otros_productos')
+    let varios = ''
+    for(let i = 0; i < data.products.length; i++){
+        varios += `<article>
+                                        <img src='${data.products[i].images[1]}' alt=${data.products[i].title}>
+                                        <div>
+                                            <h3>${data.products[i].title}</h3>
+                                            <p> ${data.products[i].description}</p> 
+                                            <a href="product.html"> <button>Ver detalle</button> </a>
+                                        </div>
+                                    </article>`
+    }
+    otros_productos.innerHTML = varios
+})
+.catch(function(error){
+    console.log(error)})
