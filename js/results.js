8a953ab48b7acaf7438b19d2ec01aa2e3b7b93b4
barberom <barberom@udesa.edu.ar>
2025-11-20 10:31:26 -0300

    results.js

---
+++ js/results.js
@@ -0,0 +1,35 @@
+let queryString = location.search
+let queryStringObj = new URLSearchParams(queryString)
+let busqueda = queryStringObj.get('busqueda')
+
+
+fetch(`https://dummyjson.com/products/search?q=${busqueda}`)
+.then(function(response){
+    return response.json()
+})
+.then(function(data){
+    let container_busqueda = document.querySelector('.container_busqueda')
+    let art_busqueda = ''
+    let titulo_busqueda = document.querySelector('#titulo_busqueda')
+    if(data.products.length > 1){
+        titulo_busqueda.innerText = `Resultados de la busqueda: ${busqueda}`
+        for(let i = 0; i < data.products.length; i++){
+            art_busqueda += `<article>
+                                <img src='${data.products[i].images[0]}' alt=${data.products[i].title}>
+                                <div>
+                                    <h3>${data.products[i].title}</h3>
+                                    <p> ${data.products[i].description}</p> 
+                                    <a href="product.html"> <button>Ver detalle</button> </a>
+                                </div>
+                            </article>`
+        }
+        container_busqueda.innerHTML = art_busqueda
+    }
+    else{
+        titulo_busqueda.innerText = `No hay resultados para el término: ${busqueda}`
+    }
+})
+    
+    .catch(function(error){
+    console.log(error)
+})
